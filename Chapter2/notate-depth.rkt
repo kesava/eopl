d1df664f3ff3f9a80266bba99ef7eab7e600872e
Kesava Mallela <kmallela@apple.com>
2019-02-15 10:42:23 -0800

    removed first variant

--- Chapter2/notate-depth.rkt
+++ Chapter2/notate-depth.rkt
@@ -1,23 +1,9 @@
-(define notate-depth-1
-  (lambda (list)
-    (define notate-single
-      (lambda (list depth)
-        (cond
-          ((or (null? list) (not (list? list))) (cons list (cons depth '())))
-          (else (notate-single (car list) (+ depth 1))))))
-    (define notate-internal
-      (lambda (list notation)
-        (cond
-          ((null? list) notation)
-          (else (cons (notate-single (car list) 0) (notate-internal (cdr list) '()))))))
-    (notate-internal list '())))
-
-;; > (notate-depth-2 '(a ((b)) (c) ((((((((d)))))))) (e) (f)))
+;; > (notate-depth '(a ((b)) (c) ((((((((d)))))))) (e) (f)))
 ;; ((a 0) (b 2) (c 1) (d 8) (e 1) (f 1))
-;; > (notate-depth-2 '(a ((b)) (c) ((((((((d)))))))) (e (f))))
+;; > (notate-depth '(a ((b)) (c) ((((((((d)))))))) (e (f))))
 ;; ((a 0) (b 2) (c 1) (d 8) ((e 0) ((f 1))))
 
-(define notate-depth-2
+(define notate-depth
   (lambda (list)
     (define notate-single
       (lambda (list depth)
